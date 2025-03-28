00cc2725fa3aea02a5a90dd4e855dd9f66ebb289
VoChinhHieu <vochinhhieu11082004@gmail.com>
2025-03-28 23:30:47 +0700

    init commit

---
+++ src/main/java/org/socius/sociuswebbackend/SociusWebBackendApplication.java
@@ -0,0 +1,13 @@
+package org.socius.sociuswebbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SociusWebBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SociusWebBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/socius/sociuswebbackend/SociusWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.socius.sociuswebbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SociusWebBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
