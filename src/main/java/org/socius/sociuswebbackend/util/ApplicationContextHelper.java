28496931239cd32baf4829db8f07cdd84db7c5cf
VoChinhHieu <vochinhhieu11082004@gmail.com>
2025-04-13 21:31:58 +0700

    add ApplicationContextHelper and EntityMappingUtil for Spring bean access and entity mapping operations

---
+++ src/main/java/org/socius/sociuswebbackend/util/ApplicationContextHelper.java
@@ -0,0 +1,34 @@
+package org.socius.sociuswebbackend.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Helper class to access Spring beans from non-Spring managed classes
+ */
+@Component
+public class ApplicationContextHelper implements ApplicationContextAware {
+
+    private static ApplicationContext context;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
+    }
+
+    /**
+     * Get a bean by class type
+     */
+    public static <T> T getBean(Class<T> beanClass) {
+        return context.getBean(beanClass);
+    }
+
+    /**
+     * Get a bean by name and class type
+     */
+    public static <T> T getBean(String name, Class<T> beanClass) {
+        return context.getBean(name, beanClass);
+    }
+}
---
+++ src/main/java/org/socius/sociuswebbackend/util/EntityMappingUtil.java
@@ -0,0 +1,57 @@
+package org.socius.sociuswebbackend.util;
+
+import org.springframework.stereotype.Component;
+import org.socius.sociuswebbackend.model.entities.*;
+
+import java.util.UUID;
+
+/**
+ * Centralized utility class for entity mapping operations to avoid ambiguity in mappers
+ */
+@Component
+public class EntityMappingUtil {
+
+    /**
+     * Generic method to map an ID to an entity
+     */
+    public <T extends BaseEntity> T mapIdToEntity(UUID id, Class<T> entityClass) {
+        if (id == null) {
+            return null;
+        }
+        try {
+            T entity = entityClass.getDeclaredConstructor().newInstance();
+            entity.setId(id);
+            return entity;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create entity instance: " + entityClass.getSimpleName(), e);
+        }
+    }
+    
+    public UserEntity mapUserIdToEntity(UUID id) {
+        return mapIdToEntity(id, UserEntity.class);
+    }
+    
+    public PositionEntity mapPositionIdToEntity(UUID id) {
+        return mapIdToEntity(id, PositionEntity.class);
+    }
+    
+    public DepartmentEntity mapDepartmentIdToEntity(UUID id) {
+        return mapIdToEntity(id, DepartmentEntity.class);
+    }
+    
+    public TeamEntity mapTeamIdToEntity(UUID id) {
+        return mapIdToEntity(id, TeamEntity.class);
+    }
+    
+    public RoleEntity mapRoleIdToEntity(UUID id) {
+        return mapIdToEntity(id, RoleEntity.class);
+    }
+    
+    public PeriodEntity mapPeriodIdToEntity(UUID id) {
+        return mapIdToEntity(id, PeriodEntity.class);
+    }
+    
+    public PermissionEntity mapPermissionIdToEntity(UUID id) {
+        return mapIdToEntity(id, PermissionEntity.class);
+    }
+}
